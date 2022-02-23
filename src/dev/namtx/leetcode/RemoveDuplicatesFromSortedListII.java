a2e551bcac24671009b00773b06369b2eaf8a4fc
Nam Tran <nam.tran.ext@personio.de>
2022-02-23 22:13:27 +0700

    [leetcode] Add https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii

--- src/dev/namtx/leetcode/RemoveDuplicatesFromSortedListII.java
+++ src/dev/namtx/leetcode/RemoveDuplicatesFromSortedListII.java
@@ -6,6 +6,7 @@ public class RemoveDuplicatesFromSortedListII {
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
         head.next = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3, new ListNode(4, new ListNode(4, new ListNode(5)))))));
+        System.out.println(new RemoveDuplicatesFromSortedListII().iterativelyDeleteDuplicates(head));
         System.out.println(new RemoveDuplicatesFromSortedListII().deleteDuplicates(head));
     }
 
@@ -25,5 +26,25 @@ public class RemoveDuplicatesFromSortedListII {
             return deleteDuplicates(head);
         }
         return head;
+    }
+
+    public ListNode iterativelyDeleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = head;
+        ListNode prev = dummy;
+
+        while (cur != null && cur.next != null) {
+            if (cur.val != cur.next.val) {
+                prev = cur;
+            } else {
+                while (cur.next != null && cur.val == cur.next.val) {
+                    cur = cur.next;
+                }
+                prev.next = cur.next;
+            }
+            cur = cur.next;
+        }
+
+        return dummy.next;
     }
 }
