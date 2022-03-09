225203833a1a2aff78851ddde7b90aec780fb448
Nam Tran <nam.tran.ext@personio.de>
2022-03-09 20:44:43 +0700

    [leetcode] Ad https://leetcode.com/problems/decode-ways/

---
+++ src/dev/namtx/leetcode/DecodeWays.java
@@ -0,0 +1,26 @@
+package dev.namtx.leetcode;
+
+public class DecodeWays {
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
+        for (int i = 2; i <= s.length(); i++) {
+            int first = Integer.parseInt(s.substring(i - 1, i));
+            int second = Integer.parseInt(s.substring(i - 2, i));
+            if (first >= 1 && first <= 9) {
+                dp[i] += dp[i - 1];
+            }
+            if (second >= 10 && second <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new DecodeWays().numDecodings("2611055971756562"));
+    }
+}
