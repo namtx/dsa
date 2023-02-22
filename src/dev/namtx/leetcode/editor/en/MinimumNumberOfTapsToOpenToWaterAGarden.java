24e61d28810e5838f9f277293aade0f1eeaf8cf7
Nam Tran <nam.tran.ext@personio.de>
2023-02-22 21:33:54 +0700

    [leetcode] Capacity to ship packages within days

---
+++ src/dev/namtx/leetcode/editor/en/MinimumNumberOfTapsToOpenToWaterAGarden.java
@@ -0,0 +1,17 @@
+package dev.namtx.leetcode.editor.en;
+
+public class MinimumNumberOfTapsToOpenToWaterAGarden {
+    public static void main(String[] args) {
+        Solution solution = new MinimumNumberOfTapsToOpenToWaterAGarden().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int minTaps(int n, int[] ranges) {
+            // TODO: Fix it
+            return 0;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
