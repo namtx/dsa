2b61a98e082761b52e26a912e91ab2781f8073fa
Nam Tran <nam.tran.ext@personio.de>
2023-02-23 23:36:24 +0700

    [leetcode] IPO

---
+++ src/dev/namtx/leetcode/editor/en/WildcardMatching.java
@@ -0,0 +1,17 @@
+package dev.namtx.leetcode.editor.en;
+
+public class WildcardMatching {
+    public static void main(String[] args) {
+        Solution solution = new WildcardMatching().new Solution();
+    }
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isMatch(String s, String p) {
+        // TODO: Fix it
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
